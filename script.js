58d05162e4974436a8fdbc0d02299db7da2c637f
Joel Warburton <Joel.s.warburton@gmail.com>
2023-12-15 23:37:02 +1100

    Implement main calculator functions and click event listener

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+let currentInput = "";
+let firstNumber;
+let secondNumber;
+let operation;
 
+document.querySelectorAll(".btn").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    clickHandler(e);
+  });
+});
+
+function clickHandler(e) {
+  console.log(e.target.classList.contains("number"));
+}
+
+function addition(num1, num2) {
+  return num1 + num2;
+}
+
+function subtraction(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplication(num1, num2) {
+  return num1 * num2;
+}
+
+function division(num1, num2) {
+  return num1 / num2;
+}
+
+function inverse(num) {
+  return -1 * num;
+}
+
+function modulo(num1, num2) {
+  return num1 % num2;
+}
