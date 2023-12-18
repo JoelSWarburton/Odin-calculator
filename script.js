ee6e4256d06762d87ec5848dc4c582f956462666
Joel Warburton <Joel.s.warburton@gmail.com>
2023-12-18 14:19:03 +1100

    Add event handlers for numbers and operands

--- script.js
+++ script.js
@@ -1,16 +1,72 @@
 let currentInput = "";
-let firstNumber;
-let secondNumber;
-let operation;
+let firstNumber = "0";
+let secondNumber = "";
+let operand = null;
+let operations = ["x", "÷", "+", "-"];
 
-document.querySelectorAll(".btn").forEach((button) => {
+document.querySelectorAll(".number").forEach((button) => {
   button.addEventListener("click", (e) => {
-    clickHandler(e);
+    addNumber(e);
   });
 });
 
-function clickHandler(e) {
-  console.log(e.target.classList.contains("number"));
+document.querySelectorAll(".operand").forEach((button) => {
+  button.addEventListener("click", (e) => {
+    addOperand(e);
+  });
+});
+
+//todo refactor code so number buttons and operand buttons have different listeners.s
+function addNumber(e) {
+  console.log("add number accessed");
+  if (firstNumber == "0") {
+    //delete zero placeholder
+    firstNumber = "";
+  }
+
+  if (!operand) {
+    firstNumber += e.target.textContent;
+  } else {
+    secondNumber += e.target.textContent;
+  }
+
+  console.log("first number: " + firstNumber);
+  console.log("second number: " + secondNumber);
+}
+
+function addOperand(e) {
+  if (!secondNumber) {
+    operand = e.target.textContent;
+    return;
+  }
+
+  //executes if secondNumber is populated
+  let result = calculate(firstNumber, secondNumber, operand);
+  firstNumber = result;
+  operand = e.target.textContent;
+  secondNumber = "";
+  console.log(result);
+}
+
+function calculate(num1, num2, op) {
+  num1 = Number.parseInt(num1);
+  num2 = Number.parseInt(num2);
+  let result;
+  switch (op) {
+    case "+":
+      result = addition(num1, num2);
+      break;
+    case "-":
+      result = subtraction(num1, num2);
+      break;
+    case "÷":
+      result = division(num1, num2);
+      break;
+    case "x":
+      result = multiplication(num1, num2);
+      break;
+  }
+  return result;
 }
 
 function addition(num1, num2) {
