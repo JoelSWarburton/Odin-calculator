7d7ebff4d872a0e6585fa5bf69c8bc58591607e9
Joel Warburton <Joel.s.warburton@gmail.com>
2023-12-26 18:42:40 +1100

    Add event listeners and handler code for calculator

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let firstNumber = "0";
 let secondNumber = "";
 let operand = null;
 let operations = ["x", "÷", "+", "-"];
+const output = document.querySelector(".output");
+const expression = document.querySelector(".expression");
 
 document.querySelectorAll(".number").forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -16,6 +18,39 @@ document.querySelectorAll(".operand").forEach((button) => {
   });
 });
 
+document.querySelector(".equals").addEventListener("click", () => {
+  if (operand && secondNumber) {
+    updateExpressionText(
+      firstNumber + " " + operand + " " + secondNumber + " ="
+    );
+    firstNumber = calculate(firstNumber, secondNumber, operand);
+
+    secondNumber = "";
+    operand = null;
+    updateOutputText(firstNumber);
+  }
+});
+
+document.querySelector(".invert").addEventListener("click", () => {
+  if (operand) {
+    if (secondNumber != "") {
+      secondNumber = String(Number(secondNumber) * -1);
+      updateOutputText(secondNumber);
+    }
+  } else {
+    firstNumber = String(Number(firstNumber) * -1);
+    updateOutputText(firstNumber);
+  }
+});
+
+document.querySelector(".cancel").addEventListener("click", () => {
+  firstNumber = "0";
+  operand = null;
+  secondNumber = "";
+  updateOutputText(firstNumber);
+  updateExpressionText("");
+});
+
 //todo refactor code so number buttons and operand buttons have different listeners.s
 function addNumber(e) {
   console.log("add number accessed");
@@ -26,8 +61,10 @@ function addNumber(e) {
 
   if (!operand) {
     firstNumber += e.target.textContent;
+    updateOutputText(firstNumber);
   } else {
     secondNumber += e.target.textContent;
+    updateOutputText(secondNumber);
   }
 
   console.log("first number: " + firstNumber);
@@ -37,21 +74,23 @@ function addNumber(e) {
 function addOperand(e) {
   if (!secondNumber) {
     operand = e.target.textContent;
-    return;
+  } else {
+    //executes if secondNumber is populated
+    let result = calculate(firstNumber, secondNumber, operand);
+    firstNumber = result;
+    operand = e.target.textContent;
+    secondNumber = "";
+    console.log(result);
+    updateOutputText(result);
   }
-
-  //executes if secondNumber is populated
-  let result = calculate(firstNumber, secondNumber, operand);
-  firstNumber = result;
-  operand = e.target.textContent;
-  secondNumber = "";
-  console.log(result);
+  updateExpressionText(firstNumber + " " + operand);
 }
 
 function calculate(num1, num2, op) {
   num1 = Number.parseInt(num1);
   num2 = Number.parseInt(num2);
   let result;
+
   switch (op) {
     case "+":
       result = addition(num1, num2);
@@ -65,7 +104,11 @@ function calculate(num1, num2, op) {
     case "x":
       result = multiplication(num1, num2);
       break;
+    case "%":
+      result = modulo(num1, num2);
+      break;
   }
+
   return result;
 }
 
@@ -91,4 +134,12 @@ function inverse(num) {
 
 function modulo(num1, num2) {
   return num1 % num2;
+}
+
+function updateOutputText(numToShow) {
+  output.textContent = numToShow;
+}
+
+function updateExpressionText(expToShow) {
+  expression.textContent = expToShow;
 }
